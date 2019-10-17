860006a840f76b065fa14c194d4a7e4ada1aecf3
Lerman <contact@lerman.fr>
2019-10-17 20:40:01 +0200

    add accepted type and max size

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/h2non/filetype"
 )
 
+const baseURL string = "https://zqsd.eu/"
+const port string = ":8090"
+const MB = 1 << 20
+
+var acceptedType = [...]string{"image/x-icon", "image/gif", "image/png", "image/jpeg", "image/vnd.adobe.photoshop", "image/tiff"}
+
 func ping(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "pong\n")
+	fmt.Fprintf(w, "pong")
 }
 
 func upload(w http.ResponseWriter, req *http.Request) {
+	req.Body = http.MaxBytesReader(w, req.Body, 10*MB)
+
 	if req.Method == "POST" {
 		file, err := FileUpload(req)
 		if err != nil {
@@ -23,9 +35,16 @@ func upload(w http.ResponseWriter, req *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("500 - Something bad happened!"))
 		} else {
-			fmt.Fprintf(w, "%s\n", file)
+			fmt.Fprintf(w, "%s%s", baseURL, file)
 		}
 	}
+}
+
+func main() {
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/upload", upload)
+
+	http.ListenAndServe(port, nil)
 }
 
 func FileUpload(r *http.Request) (string, error) {
@@ -37,7 +56,8 @@ func FileUpload(r *http.Request) (string, error) {
 	}
 	defer file.Close()
 
-	f, err := os.OpenFile(createDirectory()+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	directory := createDirectory()
+	f, err := os.OpenFile(directory+handler.Filename, os.O_WRONLY|os.O_CREATE, 0644)
 
 	if err != nil {
 		return "", err
@@ -45,14 +65,12 @@ func FileUpload(r *http.Request) (string, error) {
 
 	io.Copy(f, file)
 
-	return handler.Filename, nil
-}
+	_, err = GetFileContentType(directory + handler.Filename)
+	if err != nil {
+		return "", err
+	}
 
-func main() {
-	http.HandleFunc("/ping", ping)
-	http.HandleFunc("/upload", upload)
-
-	http.ListenAndServe(":8090", nil)
+	return directory + handler.Filename, nil
 }
 
 func randomName(size uint8) string {
@@ -72,9 +90,33 @@ func randomName(size uint8) string {
 }
 
 func createDirectory() string {
-	folderPath := "./download/" + randomName(7)
+	folderPath := "download/" + randomName(7)
 	newpath := filepath.Join(folderPath)
 	os.MkdirAll(newpath, os.ModePerm)
 
 	return folderPath + "/"
+}
+
+func GetFileContentType(filePath string) (string, error) {
+	buf, _ := ioutil.ReadFile(filePath)
+
+	kind, _ := filetype.Match(buf)
+	if kind == filetype.Unknown {
+		return "", errors.New("Unknown file type")
+	}
+
+	contentType := kind.MIME.Value
+
+	accepted := false
+	for i := 0; i < len(acceptedType); i++ {
+		if contentType == acceptedType[i] {
+			accepted = true
+		}
+	}
+
+	if accepted != true {
+		return "", errors.New("Unaccepted file format " + kind.MIME.Value)
+	}
+
+	return contentType, nil
 }
